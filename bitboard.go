8b4650f878ba1cce922e84bf43367a106f17f17f
kjda <jouda@protonmail.com>
2017-02-01 00:53:11 +0100

    refactoring

--- bitboard.go
+++ bitboard.go
@@ -19,27 +19,6 @@ package chessongo
 *
 
 ***************************************************/
-var SQUARE_TO_COORDS = [64]string{
-	"A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8",
-	"A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7",
-	"A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6",
-	"A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5",
-	"A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4",
-	"A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3",
-	"A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2",
-	"A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1",
-}
-
-var COORS_TO_SQUARE = map[string]Square{
-	"A8": Square(0), "B8": Square(1), "C8": Square(2), "D8": Square(3), "E8": Square(4), "F8": Square(5), "G8": Square(6), "H8": Square(7),
-	"A7": Square(8), "B7": Square(9), "C7": Square(10), "D7": Square(11), "E7": Square(12), "F7": Square(13), "G7": Square(14), "H7": Square(15),
-	"A6": Square(16), "B6": Square(17), "C6": Square(18), "D6": Square(19), "E6": Square(20), "F6": Square(21), "G6": Square(22), "H6": Square(23),
-	"A5": Square(24), "B5": Square(25), "C5": Square(26), "D5": Square(27), "E5": Square(28), "F5": Square(29), "G5": Square(30), "H5": Square(31),
-	"A4": Square(32), "B4": Square(33), "C4": Square(34), "D4": Square(35), "E4": Square(36), "F4": Square(37), "G4": Square(38), "H4": Square(39),
-	"A3": Square(40), "B3": Square(41), "C3": Square(42), "D3": Square(43), "E3": Square(44), "F3": Square(45), "G3": Square(46), "H3": Square(47),
-	"A2": Square(48), "B2": Square(49), "C2": Square(50), "D2": Square(51), "E2": Square(52), "F2": Square(53), "G2": Square(54), "H2": Square(55),
-	"A1": Square(56), "B1": Square(57), "C1": Square(58), "D1": Square(59), "E1": Square(60), "F1": Square(61), "G1": Square(62), "H1": Square(63),
-}
 
 //"Most significant bit" index lookup table
 var MS1BTABLE = [256]int{}
--- square.go
+++ square.go
@@ -11,3 +11,25 @@ func (s Square) rank() int {
 func (s Square) file() int {
 	return int(s % 8)
 }
+
+var SQUARE_TO_COORDS = [64]string{
+	"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+	"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+	"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+	"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+	"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+	"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+	"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+	"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+}
+
+var COORS_TO_SQUARE = map[string]Square{
+	"a8": Square(0), "b8": Square(1), "c8": Square(2), "d8": Square(3), "e8": Square(4), "f8": Square(5), "g8": Square(6), "h8": Square(7),
+	"a7": Square(8), "b7": Square(9), "c7": Square(10), "d7": Square(11), "e7": Square(12), "f7": Square(13), "g7": Square(14), "h7": Square(15),
+	"a6": Square(16), "b6": Square(17), "c6": Square(18), "d6": Square(19), "e6": Square(20), "f6": Square(21), "g6": Square(22), "h6": Square(23),
+	"a5": Square(24), "b5": Square(25), "c5": Square(26), "d5": Square(27), "e5": Square(28), "f5": Square(29), "g5": Square(30), "h5": Square(31),
+	"a4": Square(32), "b4": Square(33), "c4": Square(34), "d4": Square(35), "e4": Square(36), "f4": Square(37), "g4": Square(38), "h4": Square(39),
+	"a3": Square(40), "b3": Square(41), "c3": Square(42), "d3": Square(43), "e3": Square(44), "f3": Square(45), "g3": Square(46), "h3": Square(47),
+	"a2": Square(48), "b2": Square(49), "c2": Square(50), "d2": Square(51), "e2": Square(52), "f2": Square(53), "g2": Square(54), "h2": Square(55),
+	"a1": Square(56), "b1": Square(57), "c1": Square(58), "d1": Square(59), "e1": Square(60), "f1": Square(61), "g1": Square(62), "h1": Square(63),
+}
