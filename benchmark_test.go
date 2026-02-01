69c477615e357cdcdfad14cefba0e28611364283
Khal Jo <1508183+kjda@users.noreply.github.com>
2026-02-01 17:15:27 +0100

    improvements+tests

---
+++ benchmark_test.go
@@ -0,0 +1,99 @@
+package chessongo
+
+import "testing"
+
+func BenchmarkInitializeBoardFromFEN(b *testing.B) {
+	b.ReportAllocs()
+	fens := []string{
+		STARTING_POSITION_FEN,
+		"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+		"8/8/8/8/8/8/8/8 w - - 0 1",
+		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+	}
+
+	for _, fen := range fens {
+		fen := fen
+		board := &Board{}
+		b.Run(fen, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				if err := board.InitFromFen(fen); err != nil {
+					b.Fatalf("init fen: %v", err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkGenerateLegalMoves(b *testing.B) {
+	fens := []string{
+		STARTING_POSITION_FEN,
+		"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+		"8/8/8/8/8/8/8/8 w - - 0 1",
+		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+	}
+
+	for _, fen := range fens {
+		fen := fen
+		b.Run(fen, func(b *testing.B) {
+			board := &Board{}
+			if err := board.InitFromFen(fen); err != nil {
+				b.Fatalf("init fen: %v", err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				board.GenerateLegalMoves()
+			}
+		})
+	}
+}
+
+// Benchmarks to get a quick feel for move generation speed via perft.
+func BenchmarkPerft(b *testing.B) {
+	positions := []struct {
+		name  string
+		fen   string
+		depth int
+	}{
+		{
+			name:  "InitialDepth3",
+			fen:   STARTING_POSITION_FEN,
+			depth: 3,
+		},
+		{
+			name:  "Position2Depth3",
+			fen:   "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+			depth: 3,
+		},
+	}
+
+	for _, p := range positions {
+		p := p
+		b.Run(p.name, func(b *testing.B) {
+			benchmarkPerft(b, p.fen, p.depth)
+		})
+	}
+}
+
+func benchmarkPerft(b *testing.B, fen string, depth int) {
+	// Warm up once to set bytes and validate the position.
+	board := &Board{}
+	if err := board.InitFromFen(fen); err != nil {
+		b.Fatalf("init fen: %v", err)
+	}
+	board.GenerateLegalMoves()
+	baseNodes := perft(board, depth)
+	b.SetBytes(int64(baseNodes))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		board = &Board{}
+		if err := board.InitFromFen(fen); err != nil {
+			b.Fatalf("init fen: %v", err)
+		}
+		board.GenerateLegalMoves()
+		b.StartTimer()
+		_ = perft(board, depth)
+	}
+}
---
+++ perft_test.go
@@ -0,0 +1,170 @@
+package chessongo
+
+import (
+	"testing"
+)
+
+func perft(b *Board, depth int) uint64 {
+	if depth == 0 {
+		return 1
+	}
+
+	var nodes uint64
+	// Make a copy of moves to avoid issues with shared slices if any,
+	// though Clone() should handle it.
+	moves := make([]Move, len(b.LegalMoves))
+	copy(moves, b.LegalMoves)
+
+	for _, m := range moves {
+		nb := CloneBoard(b)
+		nb.MakeMove(m)
+		nodes += perft(&nb, depth-1)
+	}
+	return nodes
+}
+
+func TestPerftInitialPosition(t *testing.T) {
+	b := &Board{}
+	b.InitFromFen(STARTING_POSITION_FEN)
+	b.GenerateLegalMoves()
+
+	tests := []struct {
+		depth    int
+		expected uint64
+	}{
+		{1, 20},
+		{2, 400},
+		{3, 8902},
+		{4, 197281},
+		{5, 4865609},
+	}
+
+	for _, tt := range tests {
+		nodes := perft(b, tt.depth)
+		if nodes != tt.expected {
+			t.Errorf("Perft(initial, %d) = %d, expected %d", tt.depth, nodes, tt.expected)
+		}
+	}
+}
+
+func TestPerftPosition2(t *testing.T) {
+	// Kiwipete
+	fen := "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -"
+	b := &Board{}
+	b.InitFromFen(fen)
+	b.GenerateLegalMoves()
+
+	tests := []struct {
+		depth    int
+		expected uint64
+	}{
+		{1, 48},
+		{2, 2039},
+		{3, 97862},
+		{4, 4085603},
+	}
+
+	for _, tt := range tests {
+		nodes := perft(b, tt.depth)
+		if nodes != tt.expected {
+			t.Errorf("Perft(pos2, %d) = %d, expected %d", tt.depth, nodes, tt.expected)
+		}
+	}
+}
+
+func TestPerftPosition3(t *testing.T) {
+	fen := "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"
+	b := &Board{}
+	b.InitFromFen(fen)
+	b.GenerateLegalMoves()
+
+	tests := []struct {
+		depth    int
+		expected uint64
+	}{
+		{1, 14},
+		{2, 191},
+		{3, 2812},
+		{4, 43238},
+		{5, 674624},
+	}
+
+	for _, tt := range tests {
+		nodes := perft(b, tt.depth)
+		if nodes != tt.expected {
+			t.Errorf("Perft(pos3, %d) = %d, expected %d", tt.depth, nodes, tt.expected)
+		}
+	}
+}
+
+func TestPerftPosition4(t *testing.T) {
+	fen := "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
+	b := &Board{}
+	b.InitFromFen(fen)
+	b.GenerateLegalMoves()
+
+	tests := []struct {
+		depth    int
+		expected uint64
+	}{
+		{1, 6},
+		{2, 264},
+		{3, 9467},
+		{4, 422333},
+	}
+
+	for _, tt := range tests {
+		nodes := perft(b, tt.depth)
+		if nodes != tt.expected {
+			t.Errorf("Perft(pos4, %d) = %d, expected %d", tt.depth, nodes, tt.expected)
+		}
+	}
+}
+
+func TestPerftPosition5(t *testing.T) {
+	fen := "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"
+	b := &Board{}
+	b.InitFromFen(fen)
+	b.GenerateLegalMoves()
+
+	tests := []struct {
+		depth    int
+		expected uint64
+	}{
+		{1, 44},
+		{2, 1486},
+		{3, 62379},
+		{4, 2103487},
+	}
+
+	for _, tt := range tests {
+		nodes := perft(b, tt.depth)
+		if nodes != tt.expected {
+			t.Errorf("Perft(pos5, %d) = %d, expected %d", tt.depth, nodes, tt.expected)
+		}
+	}
+}
+
+func TestPerftPosition6(t *testing.T) {
+	fen := "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"
+	b := &Board{}
+	b.InitFromFen(fen)
+	b.GenerateLegalMoves()
+
+	tests := []struct {
+		depth    int
+		expected uint64
+	}{
+		{1, 46},
+		{2, 2079},
+		{3, 89890},
+		{4, 3894594},
+	}
+
+	for _, tt := range tests {
+		nodes := perft(b, tt.depth)
+		if nodes != tt.expected {
+			t.Errorf("Perft(pos6, %d) = %d, expected %d", tt.depth, nodes, tt.expected)
+		}
+	}
+}
